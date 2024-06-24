64c3f33e269854daf86a2cd6402c8fa151d01f6d
Niansyah Eko Putra <nepaka15@gmail.com>
2024-06-24 19:48:28 +0700

    connect supabase

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require("express")
+const supabase = require("@supabase/supabase-js")
 
 const app = express()
 const PORT = 3211 || process.env.PORT
 
-app.get('/', (request, response) => {
-    console.log("Api utama di akses")
+const SUPABASE_URL = "https://grehcjfmhfvqycwwuifg.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdyZWhjamZtaGZ2cXljd3d1aWZnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxOTA4MzcyMSwiZXhwIjoyMDM0NjU5NzIxfQ.J8Vh96KS4vlioo1Cty-aCxI7F4WVFm56ObgGnctXTag" 
+
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+
+app.get("/", async(request, response) => {
+    const getData = await db.from("blog").select()
+    console.log(getData)
     response.send("ini adalah route utama")
 })
 
